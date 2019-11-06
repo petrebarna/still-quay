0b39bb1305f02a1e7c2951b9fab526a4351a53eb
petrebarna <pebarna@gmail.com>
2019-11-06 07:25:39 +0000

    upd

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
-// const bodyParser = require('body-parser');
 const cors = require('cors');
+const mongoose = require('mongoose');
 
 require('dotenv').config();
 
@@ -11,5 +11,5 @@ app.use(cors());
 app.use(express.json());
 
 app.listen(port, () => {
-  console.log('Server is running on port: ${port}');
+  console.log('Server is running on port:' +  port);
 })
