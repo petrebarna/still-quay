8642ee5b2be8ef9d6fd2f5350fa0c020081c89ed
petrebarna <pebarna@gmail.com>
2019-11-14 15:41:56 +0000

    todo reviewed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import "bootstrap/dist/css/bootstrap.min.css";
 
+
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import Account from './components/Account';
